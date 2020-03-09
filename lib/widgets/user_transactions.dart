688d13257a7b7cf30a8482c77818dd6fe3d48811
“Patrick <pctennislessons@gmail.com>
2020-03-08 22:32:10 -0600

    user transactions now passes list of transactions to list transactions

--- lib/widgets/user_transactions.dart
+++ lib/widgets/user_transactions.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../models/transaction.dart';
 import './new_transaction.dart';
 import './transaction_list.dart';
 
@@ -6,14 +7,27 @@ class UserTransactions extends StatefulWidget {
   @override
   _UserTransactionsState createState() => _UserTransactionsState();
 }
-
+  final List<Transaction> _userTransactions = [
+    Transaction(
+      id: 't1',
+      title: 'fish',
+      amount: 200.00,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't1',
+      title: 'apple',
+      amount: 2.00,
+      date: DateTime.now(),
+    ),
+  ];
 class _UserTransactionsState extends State<UserTransactions> {
   @override
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
         NewTrasaction(),
-        TransactionList(),
+        TransactionList(_userTransactions),
       ],
     );
   }
